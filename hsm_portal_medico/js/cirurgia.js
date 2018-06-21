971b3c2a77dfdcf1db7b9b1666fbcae6f0b9db01
ivanvalmorbida <ivanvalmorbida@gmail.com>
2018-06-21 08:44:02 -0300

    ivan

--- hsm_portal_medico/js/cirurgia.js
+++ hsm_portal_medico/js/cirurgia.js
@@ -68,7 +68,6 @@
 
         /*parseDate(date) {
             if (!date) return null
-
             const [month, day, year] = date.split('/')
             return `${year}-${month.padStart(2, '0')}-${day.padStart(2, '0')}`
         }*/
--- hsm_portal_medico/js/paciente.js
+++ hsm_portal_medico/js/paciente.js
@@ -159,7 +159,6 @@
 
         /*parseDate(date) {
             if (!date) return null
-
             const [month, day, year] = date.split('/')
             return `${year}-${month.padStart(2, '0')}-${day.padStart(2, '0')}`
         }*/
