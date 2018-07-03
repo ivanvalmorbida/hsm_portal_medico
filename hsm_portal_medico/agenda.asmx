fb0871c8ef05a2991c9e3e56ec4a321d40645aad
ivanvalmorbida <ivanvalmorbida@gmail.com>
2018-07-02 21:51:55 -0300

    ivan

---
+++ hsm_portal_medico/agenda.asmx
@@ -0,0 +1,1 @@
+﻿<%@ WebService Language="C#" Class="hsm_portal_medico.agenda" %>
---
+++ hsm_portal_medico/agenda.asmx.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Web;
+using System.Web.Services;
+
+namespace hsm_portal_medico
+{
+    public class agenda : System.Web.Services.WebService
+    {
+
+    }
+}
